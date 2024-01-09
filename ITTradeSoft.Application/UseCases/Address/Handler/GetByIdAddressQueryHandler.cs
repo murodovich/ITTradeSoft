3d368ca20dac98a93d83f946b925fe5eb2eafaec
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2024-01-09 20:44:06 +0500

    asasa

---
+++ ITTradeSoft.Application/UseCases/Address/Handler/GetByIdAddressQueryHandler.cs
@@ -0,0 +1,25 @@
+﻿using ITTradeSoft.Application.Absreactions;
+using ITTradeSoft.Application.UseCases.Address.Query;
+using ITTradeSoft.Domain.Entities.Address;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace ITTradeSoft.Application.UseCases.Address.Handler
+{
+    public class GetByIdAddressQueryHandler : IRequestHandler<GetByIdAddressQuery, Addres>
+    {
+        private readonly ITradeSoftDBContext _dbContext;
+
+        public GetByIdAddressQueryHandler(ITradeSoftDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<Addres> Handle(GetByIdAddressQuery request, CancellationToken cancellationToken)
+        {
+            var result = await _dbContext.Addreses.FirstOrDefaultAsync(x => x.Id == request.Id);
+            if (result == null) throw new Exception();
+            return result;         
+        }
+    }
+}
