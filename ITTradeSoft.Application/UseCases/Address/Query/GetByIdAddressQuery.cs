cd8ec75323e19bc95515b9b5c67bfbb598de7889
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2024-01-09 20:44:11 +0500

    sasa

---
+++ ITTradeSoft.Application/UseCases/Address/Query/GetByIdAddressQuery.cs
@@ -0,0 +1,10 @@
+﻿using ITTradeSoft.Domain.Entities.Address;
+using MediatR;
+
+namespace ITTradeSoft.Application.UseCases.Address.Query
+{
+    public class GetByIdAddressQuery : IRequest<Addres>
+    {
+        public int Id { get; set; }
+    }
+}
