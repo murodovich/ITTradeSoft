c4a51c4d2bba811d66f36af6ba54678f479833d7
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2024-01-09 20:44:15 +0500

    sasas

---
+++ ITTradeSoft.Application/UseCases/ProjectCalculations/Handler/GetAllProjectCalculationQueryHandler.cs
@@ -0,0 +1,24 @@
+﻿using ITTradeSoft.Application.Absreactions;
+using ITTradeSoft.Application.UseCases.ProjectCalculations.Query;
+using ITTradeSoft.Domain.Entities.ProjectCalculations;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace ITTradeSoft.Application.UseCases.ProjectCalculations.Handler
+{
+    public class GetAllProjectCalculationQueryHandler : IRequestHandler<GetAllProjectCalculationQuery, List<ProjectCalculator>>
+    {
+        private readonly ITradeSoftDBContext _dbContext;
+
+        public GetAllProjectCalculationQueryHandler(ITradeSoftDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<List<ProjectCalculator>> Handle(GetAllProjectCalculationQuery request, CancellationToken cancellationToken)
+        {
+            var result = await _dbContext.ProjectCalculations.ToListAsync();
+            return result;
+        }
+    }
+}
