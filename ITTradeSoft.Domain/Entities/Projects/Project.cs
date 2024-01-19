1d03e85ce70ac0b605084a4543211ad7305f8df6
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2024-01-19 19:34:19 +0500

    Project

---
+++ ITTradeSoft.Domain/Entities/Projects/Project.cs
@@ -0,0 +1,12 @@
+﻿namespace ITTradeSoft.Domain.Entities.Projects
+{
+    public class Project
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ImagePath { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set;}
+    }
+}
