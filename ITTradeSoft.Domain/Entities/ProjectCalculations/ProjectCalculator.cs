eb34055a86baea033bde43bb2bde0ff3b6ce73c9
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2024-01-19 19:34:09 +0500

    ProjectCalculator

---
+++ ITTradeSoft.Domain/Entities/ProjectCalculations/ProjectCalculator.cs
@@ -0,0 +1,12 @@
+﻿namespace ITTradeSoft.Domain.Entities.ProjectCalculations
+{
+    public class ProjectCalculator
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string FilePath { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set;}
+    }
+}
