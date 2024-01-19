1201ed6680c2fd921b50fde1bfaa5a54b63ee45c
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2024-01-19 19:34:25 +0500

    Questions

---
+++ ITTradeSoft.Domain/Entities/Question/Questions.cs
@@ -0,0 +1,13 @@
+﻿namespace ITTradeSoft.Domain.Entities.Question
+{
+    public class Questions
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Questionies { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set;}
+
+    }
+}
