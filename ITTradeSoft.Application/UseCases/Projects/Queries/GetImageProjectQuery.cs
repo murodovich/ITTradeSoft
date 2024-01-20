b3f0f2c03b6682cf3a0bec08dc659e98c5880973
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2024-01-21 04:46:39 +0500

    sasa

---
+++ ITTradeSoft.Application/UseCases/Projects/Queries/GetImageProjectQuery.cs
@@ -0,0 +1,10 @@
+﻿using ITTradeSoft.Domain.Entities.Projects;
+using MediatR;
+
+namespace ITTradeSoft.Application.UseCases.Projects.Queries
+{
+    public class GetImageProjectQuery : IRequest<string>
+    {
+        public string ImagePath { get; set; }
+    }
+}
