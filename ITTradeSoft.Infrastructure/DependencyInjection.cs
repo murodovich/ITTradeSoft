33a6971bdb1fe6a702c94571d5d3fc706890b07d
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2024-01-20 12:16:10 +0500

    asasa

---
+++ ITTradeSoft.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,21 @@
+﻿using ITTradeSoft.Application.Absreactions;
+using ITTradeSoft.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ITTradeSoft.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ITradeSoftDBContext,TradeSoftDBContext>(options =>
+            {
+                options.UseNpgsql(configuration.GetConnectionString("Defoult"));
+            });
+
+            return services;
+        }
+    }
+}
