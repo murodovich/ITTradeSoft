870d285ebbd53c68c916bc2e33f7f8dfa78cb6cd
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2024-01-21 04:46:27 +0500

    sasasa

---
+++ ITTradeSoft.Application/UseCases/Projects/Handler/GetAllProjectQueryHandler.cs
@@ -0,0 +1,24 @@
+﻿using ITTradeSoft.Application.Absreactions;
+using ITTradeSoft.Application.UseCases.Projects.Queries;
+using ITTradeSoft.Domain.Entities.Projects;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace ITTradeSoft.Application.UseCases.Projects.Handler
+{
+    public class GetAllProjectQueryHandler : IRequestHandler<GetAllProjectQuery, List<Project>>
+    {
+        private readonly ITradeSoftDBContext _dbContext;
+
+        public GetAllProjectQueryHandler(ITradeSoftDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<List<Project>> Handle(GetAllProjectQuery request, CancellationToken cancellationToken)
+        {
+            var result = await _dbContext.Projects.ToListAsync(cancellationToken);
+            return result;
+        }
+    }
+}
---
+++ ITTradeSoft.Application/UseCases/Projects/Handler/GetImageProjectQueryHandler.cs
@@ -0,0 +1,30 @@
+﻿using ITTradeSoft.Application.Absreactions;
+using ITTradeSoft.Application.FileServices;
+using ITTradeSoft.Application.UseCases.Projects.Queries;
+using ITTradeSoft.Domain.Entities.Projects;
+using ITTradeSoft.Domain.Exceptions.ImageExceptions;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace ITTradeSoft.Application.UseCases.Projects.Handler
+{
+    public class GetImageProjectQueryHandler : IRequestHandler<GetImageProjectQuery, string>
+    {
+        private readonly ITradeSoftDBContext _dbContext;
+        private readonly IFileService _fileService;
+
+        public GetImageProjectQueryHandler(ITradeSoftDBContext dbContext, IFileService fileService)
+        {
+            _dbContext = dbContext;
+            _fileService = fileService;
+        }
+
+        public async Task<string> Handle(GetImageProjectQuery request, CancellationToken cancellationToken)
+        {
+            await _fileService.GetImageAsync(request.ImagePath);
+            return string.Empty;
+            
+            
+        }
+    }
+}
