810d6266e410124a6881b85d26e4cb2423ebbb4c
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2024-01-20 12:15:41 +0500

    asas

---
+++ ITTradeSoft.Application/Absreactions/ITradeSoftDBContext.cs
@@ -0,0 +1,16 @@
+﻿using ITTradeSoft.Domain.Entities.ProjectCalculations;
+using ITTradeSoft.Domain.Entities.Projects;
+using ITTradeSoft.Domain.Entities.Question;
+using Microsoft.EntityFrameworkCore;
+
+namespace ITTradeSoft.Application.Absreactions
+{
+    public interface ITradeSoftDBContext
+    {
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<Questions> Questions { get; set; }
+        public DbSet<ProjectCalculator> ProjectCalculations { get; set; }
+
+        public Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}
