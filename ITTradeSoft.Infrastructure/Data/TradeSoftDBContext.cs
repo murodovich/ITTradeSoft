97963e846b883017bf4a914e2bef5a4552e7b9a8
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2024-01-20 12:15:55 +0500

    sasasa

---
+++ ITTradeSoft.Infrastructure/Data/TradeSoftDBContext.cs
@@ -0,0 +1,23 @@
+﻿using ITTradeSoft.Application.Absreactions;
+using ITTradeSoft.Domain.Entities.ProjectCalculations;
+using ITTradeSoft.Domain.Entities.Projects;
+using ITTradeSoft.Domain.Entities.Question;
+using Microsoft.EntityFrameworkCore;
+
+namespace ITTradeSoft.Infrastructure.Data
+{
+    public class TradeSoftDBContext : DbContext,ITradeSoftDBContext
+    {
+        public TradeSoftDBContext(DbContextOptions<TradeSoftDBContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<Questions> Questions { get; set; }
+        public DbSet<ProjectCalculator> ProjectCalculations { get; set; }
+
+
+
+    }
+}
