010a5e5e144e47ba93e867ae6a88646cea1e9abd
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2024-01-21 01:09:54 +0500

    save

---
+++ ITTradeSoft.Api/Controllers/ProjectsController.cs
@@ -0,0 +1,26 @@
+﻿using ITTradeSoft.Application.UseCases.Projects.Commands;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ITTradeSoft.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProjectsController : ControllerBase
+    {
+
+        private readonly IMediator _mediator;
+
+        public ProjectsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        public async ValueTask<IActionResult> CreateProjectAsync([FromForm]CreateProjectCommand createProject)
+        {
+            await _mediator.Send(createProject);
+            return Ok("created");
+        }
+    }
+}
--- ITTradeSoft.Api/Program.cs
+++ ITTradeSoft.Api/Program.cs
@@ -1,4 +1,5 @@
 using ITTradeSoft.Application;
+using ITTradeSoft.Application.FileServices;
 using ITTradeSoft.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,6 +10,8 @@ builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IFileService,FileService>();
+
 
 
 var app = builder.Build();
