7f410c76078d58ab4b52aeadaa269070d1a5ffaa
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2024-01-21 04:46:51 +0500

    asasa

---
+++ ITTradeSoft.Application/UseCases/Questions/Command/UpdateQuestionsCommand.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+
+namespace ITTradeSoft.Application.UseCases.Questions.Command
+{
+    public class UpdateQuestionsCommand : IRequest<bool>
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Questionies { get; set; }
+        public DateTime Updated { get; set; } = DateTime.Now;
+    }
+}
