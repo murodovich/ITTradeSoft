bfb21119c3af867d8ffc78997f65fcac5e55a036
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2024-01-21 04:47:09 +0500

    asasas

---
+++ ITTradeSoft.Application/UseCases/Questions/Handler/UpdatedQuestionsCommandHandler.cs
@@ -0,0 +1,21 @@
+﻿using ITTradeSoft.Application.Absreactions;
+using ITTradeSoft.Application.UseCases.Questions.Command;
+using MediatR;
+
+namespace ITTradeSoft.Application.UseCases.Questions.Handler
+{
+    public class UpdatedQuestionsCommandHandler : IRequestHandler<UpdateQuestionsCommand, bool>
+    {
+        private readonly ITradeSoftDBContext _dbContext;
+
+        public UpdatedQuestionsCommandHandler(ITradeSoftDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public Task<bool> Handle(UpdateQuestionsCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
