024576266f4739798925ce7a500f6fdfde47f49b
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2024-01-21 04:46:42 +0500

    asasss

---
+++ ITTradeSoft.Application/UseCases/Questions/Command/CreateQuestionsCommand.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+
+namespace ITTradeSoft.Application.UseCases.Questions.Command
+{
+    public class CreateQuestionsCommand : IRequest<bool>
+    {
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Questionies { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+    }
+}
