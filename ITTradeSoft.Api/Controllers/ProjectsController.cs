04f9424f06e308122e6e013f59a0ec668ace8cb4
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2024-01-22 15:10:17 +0500

    asasa

--- ITTradeSoft.Api/Controllers/ProjectsController.cs
+++ ITTradeSoft.Api/Controllers/ProjectsController.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace ITTradeSoft.Api.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/[controller]/[action]")]
     [ApiController]
     public class ProjectsController : ControllerBase
     {
