b6d0a9fde88b91ab29bac63528f0192160acfdd3
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2024-01-22 21:30:06 +0500

    sasa

---
+++ ITTradeSoft.Api/Controllers/ProjectCalculationController.cs
@@ -0,0 +1,25 @@
+﻿using ITTradeSoft.Application.UseCases.ProjectCalculations.Command;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ITTradeSoft.Api.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class ProjectCalculationController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public ProjectCalculationController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        [HttpPost]
+        public async ValueTask<IActionResult> CreateProjectCalculatorAsync([FromForm]CreateProjectCalculationCommand command)
+        {
+            await _mediator.Send(command);
+            return Ok("Created");
+        }
+
+    }
+}
