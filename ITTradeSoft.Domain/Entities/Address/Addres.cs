a1e0600d6f80e6169b2abd726f2c8dd4e65096e8
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2024-01-22 15:10:28 +0500

    asasas

---
+++ ITTradeSoft.Domain/Entities/Address/Addres.cs
@@ -0,0 +1,12 @@
+﻿namespace ITTradeSoft.Domain.Entities.Address
+{
+    public class Addres
+    {
+        public int Id { get; set; }
+        public string PhoneNumber { get; set; }
+        public string City { get; set; }
+        public string Email { get; set; }
+
+
+    }
+}
